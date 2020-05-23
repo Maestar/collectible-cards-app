073e6be9cd6de8c8ebe6ae5a56bfa55361a7961d
Tera <terabenoit@gmail.com>
2020-05-22 19:16:05 -0500

    added Tilty library, made a first pass at styling. note to add a rarity prop later.

--- src/Components/Card/Card.component.jsx
+++ src/Components/Card/Card.component.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import './Card.styles.css';
+import Tilty from "react-tilty";
 
 
 class Card extends React.Component {
 
   render() {
+    //NOTE:add a rarity prop and check rarity to determine what to render.
+    //this lets us change how the card will look based on rarity.
       const{name, image, desc} = this.props.card;
     return (
-      <div className="card">
+
+      <Tilty className="card" >
         <h4 className="card-title">{name}</h4>
         <img className="card-image" alt='cardImg' src={image}/>
         <p className="card-desc">{desc}</p>
-      </div>
+      </Tilty>
+
     );
   }
 }
--- src/cardList.js
+++ src/cardList.js
@@ -2,81 +2,81 @@ const cardList = {
     rareList:{
         1: {
             name: "Test Card Rare 1",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         2: {
             name: "Test Card Rare 2",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         3: {
             name: "Test Card Rare 3",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         4: {
             name: "Test Card Rare 4",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         5: {
             name: "Test Card Rare 5",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         }
     },
     uncommonList:{
         1: {
             name: "Test Card Uncommon 1",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         2: {
             name: "Test Card Uncommon 2",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         3: {
             name: "Test Card Uncommon 3",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         4: {
             name: "Test Card Uncommon 4",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         5: {
             name: "Test Card Uncommon 5",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         }
     },
     commonList:{
         1: {
             name: "Test Card Common 1",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         2: {
             name: "Test Card Common 2",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         3: {
             name: "Test Card Common 3",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         4: {
             name: "Test Card Common 4",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         },
         5: {
             name: "Test Card Common 5",
-            image: "/images/testcard.png",
+            image: "/images/card-image-001.png",
             desc: "This is the card description, card information and flavor text goes here",
         }
     }
